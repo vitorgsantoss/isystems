6c32d8d52ca7c3087015c2a956fdeda048b1813e
vitorgsantoss <vg452004@gmail.com>
2025-12-30 14:50:00 -0300

    BUILD: products app added

--- tenants/admin.py
+++ tenants/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from tenants.models import Tenant, Adress
 
-# Register your models here.
+
+class AdressInline(admin.TabularInline):
+    model = Adress
+    extra = 1
+
+
+@admin.register(Tenant)
+class TenantAdmin(admin.ModelAdmin):
+    inlines = [AdressInline]
--- tenants/models.py
+++ tenants/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tenant(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, related_name='tenant', on_delete=models.CASCADE)
+    cnpj = models.CharField(max_length=20, blank=True, null=True)
+    active = models.BooleanField(default=False)
+    
+
+    def __str__(self):
+        return self.name
+
+
+class Adress(models.Model):
+    tenant = models.ForeignKey(Tenant, verbose_name='adress', on_delete=models.CASCADE)
+    street = models.CharField(max_length=255)
+    number = models.CharField(max_length=7)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f' Adress of {self.tenant}'
